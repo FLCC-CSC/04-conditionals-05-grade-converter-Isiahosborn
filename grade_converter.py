79b108e581f669444e3f8cf5be4003d028adee39
Isiahosborn <iosborn@fingerlakes.edu>
2025-10-05 14:48:14 -0400

    Update grade_converter.py

--- grade_converter.py
+++ grade_converter.py
@@ -1,36 +1,30 @@
 # FILE NAME - grade_converter.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
-
-
-
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions and Attestation below
-# 4. The Sample Output has been included in this code for your convenience
-
-
+# NAME: Isiah Osborn
+# DATE: 2025-10-05
+# BRIEF DESCRIPTION:
+#   Read a numerical grade and print the corresponding letter grade.
+#   >100 => A+, 90–100 => A, 80–89 => B, 70–79 => C, 65–69 => D, else F.
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
-print('===== Grade Converter =====')
-
-percent = int(input('Enter a numerical grade (1-100): '))
+print("===== Grade Converter =====")
+percent = int(input("Enter a numerical grade (1-100): ").strip())
 
 if percent > 100:
-    print('A+')
+    print("A+")
 elif percent >= 90:
-    print('A')
+    print("A")
 elif percent >= 80:
-    print('B')
+    print("B")
 elif percent >= 70:
-    print('C')
+    print("C")
 elif percent >= 65:
-    print('D')
+    print("D")
 else:
-    print('F')
+    print("F")
+
+########### END YER CODE ABOVE THIS LINE ###########
 
 ########### END YER CODE ABOVE THIS LINE ###########
 
